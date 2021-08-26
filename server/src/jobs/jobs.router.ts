756bb6f371649e6dffab271883cde018b481b599
koleCar <h.kolaric92@gmail.com>
2021-08-26 15:00:24 +0200

    fix(jobs.router.ts): Changed name to query

--- server/src/jobs/jobs.router.ts
+++ server/src/jobs/jobs.router.ts
@@ -15,7 +15,7 @@ router.get(
 
     // @ts-ignore
     const account: string = req['account'] || req.query.account;
-    const {name} = req.params;
+    const {name} = req.query;
     const page = req.query.page ? parseInt(req.query.page as string, 10) : 0;
     const size = req.query.size ? parseInt(req.query.size as string, 10) : 10;
     const sort = {
