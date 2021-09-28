aa2a5490b550ffdc73d4c03fcf2e67cb8116082d
koleCar <46318267+koleCar@users.noreply.github.com>
2021-09-28 15:24:29 +0200

    feat(server): support for reviving jobs

--- server/src/config.ts
+++ server/src/config.ts
@@ -8,6 +8,12 @@ export const CONFIG = {
     initialAccountPassword: process.env.USER_PASSWORD as string,
     secret: process.env.SECRET as string
   },
+  startupStrategy: {
+    useStartup: process.env.USE_STARTUP || true,
+    /*In minutes*/
+    lowerLimit: parseFloat(process.env.LOWER_LIMIT || '10'),
+    upperLimit: parseFloat(process.env.UPPER_LIMIT || '60'),
+  },
   apiKey: process.env.API_KEY as string,
   runnerId: process.env.RUNNER_ID as string
 };
