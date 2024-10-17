00aebdb2f60dad555616390c2667140acbc7833d
koleCar <h.kolaric92@gmail.com>
2024-10-17 10:20:20 +0200

    fix: fronted job route

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,16 +1,24 @@
 import { Routes } from '@angular/router';
+import { AccountsComponent } from './pages/accounts/accounts.component';
+import { JobsComponent } from './pages/jobs/jobs.component';
+import { LoginComponent } from './pages/login/login.component';
 import {AuthGuard} from './shared/guards/auth.guard';
 
 export const routes: Routes = [
-  { path: '', loadComponent: () => import('./pages/login/login.component').then(mod => mod.LoginComponent) },
+  { path: '',
+    component: LoginComponent,
+    // loadComponent: () => import('./pages/login/login.component').then(mod => mod.LoginComponent)
+  },
   {
     path: 'accounts',
-    loadComponent: () => import('./pages/accounts/accounts.component').then(mod => mod.AccountsComponent),
+    component: AccountsComponent,
+    // loadComponent: () => import('./pages/accounts/accounts.component').then(mod => mod.AccountsComponent),
     canActivate: [AuthGuard]
   },
   {
     path: 'accounts/:account/jobs',
-    loadChildren: () => import('./pages/jobs/jobs.component').then(mod => mod.JobsComponent),
+    component: JobsComponent,
+    // loadChildren: () => import('./pages/jobs/jobs.component').then(mod => mod.JobsComponent),
     canActivate: [AuthGuard]
   },
   {
